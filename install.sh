5f70133ada95b57595464e2c3a782edd959eb7b9
Dvd-Znf <101937630+Dvd-Znf@users.noreply.github.com>
2024-02-26 16:31:55 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ mkdir ~/.cache/youtty/data
 #touch ~/.youtty/data/history Now using cache dir ~/.cache/youtty
 touch ~/.cache/youtty/data/kitten
 echo "0" > ~/.cache/youtty/data/kitten
+mkdir ~/.youtty
 cp ./api-caller.py ~/.youtty/
 cp ./key_name.py ~/.youtty/
 sudo make install
